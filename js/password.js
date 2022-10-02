1f87b61f1afd3dcd4853b0674cfb18ddc0d9896f
ErickSolisR16 <111633858+ErickSolisR16@users.noreply.github.com>
2022-10-02 17:39:48 -0600

    Creation of the function that obtains the type and size of the password and calls the corresponding function. Corrections in the password creation functions

--- js/password.js
+++ js/password.js
@@ -2,62 +2,83 @@
 var btnGenerate = document.getElementById("generate");
 // Password
 var finalPassword = "";
-
+// Input where you put the created password
 var inputPassword = document.getElementById("password");
+// Parts of the final password are saved
+var temporalPassword;
 
-// Create letter password function
-function lettersPassword() {
+/**
+ * * Function that redirects to the type and size that the passwords should be
+ */
+function password() {
+    var typePassword = document.getElementById("type").value;
+    var lenghtPassword = document.getElementById("lenght").value;
     inputPassword.value = "";
     finalPassword = "";
-    var lenghtPassword = document.getElementById("lenght").value;
+    if (typePassword == 0) {
+        lettersPassword(lenghtPassword);
+    } else if (typePassword == 1) {
+        numbersPassword(lenghtPassword);
+    } else if (typePassword == 2) {
+        lettersNumbersPassword(lenghtPassword);
+    } else {
+        allCaractersPassword(lenghtPassword);
+    }
+}
+
+/**
+ * * Create letter password function
+ * 
+ * @param {*} lenght of the password
+ */
+function lettersPassword(lenght) {
     var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    var temporalPassword = "";
-    for (var i = 0; i < lenghtPassword; i++) {
+    temporalPassword = "";
+    for (var i = 0; i < lenght; i++) {
         temporalPassword = Math.floor(Math.random() * str.length + 1);
         finalPassword += str.charAt(temporalPassword);
     }
     inputPassword.value = finalPassword;
-    console.log(finalPassword);
 }
 
-// Create numbers password function
-function numbersPassword() {
-    inputPassword.value = "";
-    finalPassword = "";
-    var lenghtPassword = document.getElementById("lenght").value;
-    for (var i = 0; i < lenghtPassword; i++) { 
+/**
+ * * Create numbers password function
+ * 
+ * @param {*} lenght of the password
+ */
+function numbersPassword(lenght) {
+    for (var i = 0; i < lenght; i++) { 
         finalPassword += Math.floor(Math.random() * 10);
     }
     inputPassword.value = finalPassword;
-    console.log(finalPassword);
 } 
 
-// Create numbers and letters password function
-function lettersNumbersPassword() {
-    inputPassword.value = "";
-    finalPassword = "";
-    var lenghtPassword = document.getElementById("lenght").value;
+/**
+ * * Create numbers and letters password function
+ * 
+ * @param {*} lenght of the password
+ */
+function lettersNumbersPassword(lenght) {
     var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var temporalPassword = "";
-    for (var i = 0; i <  lenghtPassword; i++) {
+    temporalPassword = "";
+    for (var i = 0; i <  lenght; i++) {
         temporalPassword = Math.floor(Math.random() * str.length + 1); 
         finalPassword += str.charAt(temporalPassword);
-        console.log(finalPassword);
     }
     inputPassword.value = finalPassword;
-    console.log(finalPassword);
 }
 
-function allCaractersPassword() {
-    inputPassword.value = "";
-    finalPassword = "";
-    var lenghtPassword = document.getElementById("lenght").value;
+/**
+ * * Create all caracters password function
+ * 
+ * @param {*} lenght of the password
+ */
+function allCaractersPassword(lenght) {
     var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789¡!¿?#$%^&*()_-+=[]''|/<>,.";
-    var temporalPassword = "";
-    for (var i = 0; i < lenghtPassword; i++) {
+    temporalPassword = "";
+    for (var i = 0; i < lenght; i++) {
         temporalPassword = Math.floor(Math.random() * str.length + 1); 
         finalPassword += str.charAt(temporalPassword);
-        console.log(finalPassword);
     }
     inputPassword.value = finalPassword;
 }
